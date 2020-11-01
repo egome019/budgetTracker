09f3278d8df3b9628167658f8232c8af299cd54c
erik G <egomezfax19@gmail.com>
2020-10-31 22:13:08 -0700

    created manifest and sw.js

---
+++ public/service-worker.js
@@ -0,0 +1,6 @@
+const CACHE = "static-cache-v1";
+const DATA_CACHE = "data-cache-v1";
+
+const iconSizes = ["192", "512"];
+const iconFiles = iconSizes.map((size) => `/icons/icon-${size}x${size}.png`);
+
