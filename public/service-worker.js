699c9c092cb33bbbe095ddd89664a3552745a36d
erik G <egomezfax19@gmail.com>
2020-11-01 20:32:27 -0800

    completed service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,3 +4,61 @@ const DATA_CACHE = "data-cache-v1";
 const iconSizes = ["192", "512"];
 const iconFiles = iconSizes.map((size) => `/icons/icon-${size}x${size}.png`);
 
+const staticFilesToCache = [
+    "/",
+    "index.js",
+    "manifest.webmanifest"
+].concat(iconFiles);
+
+self.addEventListener("install", function(event) {
+    event.waitUntil(
+        caches.open(CACHE).then(cache => {
+            console.log("Your files were pre-cached successfully!");
+            return cache.addAll(staticFilesToCache);
+        })
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", function(event) {
+    event.waitUntil(
+        caches.keys().then(keyList => {
+            return Promise.all(keyList.map(key => {
+                if(key !== CACHE && key !== DATA_CACHE) {
+                    console.log("Removing old cache data", key);
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+    self.clients.claim();
+});
+
+self.addEventListener("fetch", function(event) {
+    const {url} = event.request;
+    if (url.includes("/all") || url.includes("/find")) {
+        event.respondWith(
+            caches.open(DATA_CACHE).then(cache => {
+                return fetch(event.request)
+                    .then(response => {
+                        if (response.status === 200) {
+                            cache.put(event.request, response.clone());
+                        }
+
+                        return response;
+                    })
+                    .catch(err => {
+                        return cachematch(event.request);
+                    });
+            }).catch(err => console.log(err))
+        );
+    }else{
+        event.respondWith(
+            caches.open(CACHE).then(cache => {
+                return cache.match(event.request).then(response => {
+                    return response || fetch(event.request);
+                });
+            })
+        );
+    }
+});
