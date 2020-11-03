5b933cf5a0118c274f1e5b5bbd8c4c669171866c
erik G <egomezfax19@gmail.com>
2020-11-02 21:04:51 -0800

    service worker is functional

--- public/service-worker.js
+++ public/service-worker.js
@@ -36,22 +36,23 @@ self.addEventListener("activate", function(event) {
 
 self.addEventListener("fetch", function(event) {
     const {url} = event.request;
-    if (url.includes("/all") || url.includes("/find")) {
+    if (url.includes("/api/")) {
         event.respondWith(
             caches.open(DATA_CACHE).then(cache => {
                 return fetch(event.request)
                     .then(response => {
                         if (response.status === 200) {
-                            cache.put(event.request, response.clone());
+                            cache.put(url, response.clone());
                         }
 
                         return response;
                     })
                     .catch(err => {
-                        return cachematch(event.request);
+                        return cache.match(event.request);
                     });
             }).catch(err => console.log(err))
         );
+        return;
     }else{
         event.respondWith(
             caches.open(CACHE).then(cache => {
