3aff5d24c9ee8db7086820ca03441495a4651835
erik G <egomezfax19@gmail.com>
2020-11-04 07:33:10 -0800

    created db.js for record function when offline

--- public/db.js
+++ public/db.js
@@ -1,3 +1,59 @@
 let db;
 
 const request = indexedDB.open("budget", 1);
+
+request.onupgradeneeded = function(evt) {
+    const db = evt.target.result;
+    db.createObjectStore("pendiing", {autoIncrement: true});
+};
+
+request.onsuccess = function(evt) {
+    db = evt.target.result;
+
+    if(navigator.onLine) {
+        checkDb();
+    }
+};
+
+request.onerror = function(evt) {
+    console.log("Error: " + evt.target.errorCode);
+};
+
+function record(record) {
+    const transaction = db.transaction(["pending"], "readwrite");
+
+    const store = transaction.objectStore("pending");
+
+    store.add(record);
+}
+
+function checkDb() {
+    const transaction = db.transaction(["pending"], "readwrite");
+
+    const store = transaction.objectStore("pending");
+
+    const getAll = store.getAll();
+
+    getAll.onsuccess = function(){
+        if (getAll.result.length > 0) {
+            fetch("/api/transaction/bulk", {
+                method: "POST",
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: "application/json, text/plain, */*",
+                    "Content-Type": "application/json"
+                }
+            })
+            .then(reesponse => response.json())
+            .then(() => {
+                const transaction = db.transaction(["pending"], "readwrite");
+
+                const store = transaction.objectStore("pending");
+
+                store.clear();
+            });
+        }
+    };
+}
+
+window.addEventListener("online", checkDb);
--- public/index.js
+++ public/index.js
@@ -136,7 +136,7 @@ function sendTransaction(isAdding) {
   })
   .catch(err => {
     // fetch failed, so save in indexed db
-    saveRecord(transaction);
+    record(transaction);
 
     // clear form
     nameEl.value = "";
